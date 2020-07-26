b1bebce6a8166921dded7d214c4d5b3c26fc34b1
AleRojas27 <alejandro.rrc99@hotmail.com>
2020-07-25 20:09:30 -0500

    Formulario HTML

---
+++ modulos/formulario.php
@@ -0,0 +1,236 @@
+<div class="container">
+
+    <div>
+
+        <h5> 
+        
+            <span class="float-right"> <i class="fas fa-times"></i> </span>
+            <b> AGREGAR OTRA DIRECCION </b> 
+        
+        </h5>
+
+    </div>
+
+    <hr>
+
+    <div class="row">
+    
+        <div class="col-6">
+        
+            <p class="text-muted">Nombre</p>
+
+            <input type="text" class="form-control inputForm" placeholder="Escribe tu nombre...">
+
+        </div>
+
+        <div class="col-6">
+
+            <p class="text-muted">Apellidos</p>
+
+            <input type="text" class="form-control inputForm" placeholder="Escribe tus apellidos...">
+        
+        </div>
+
+    </div>
+
+    <div class="row">
+    
+        <div class="col-6">
+        
+            <p class="text-muted">Telefono Celular</p>
+
+            <input type="text" class="form-control inputForm" placeholder="Escribe tu cel...">
+
+        </div>
+
+        <div class="col-6">
+
+            <p class="text-muted">Otro telefono (Opcional)</p>
+
+            <input type="text" class="form-control inputForm" placeholder="Otro...">
+        
+        </div>
+    
+    </div>
+
+    <div class="form-inline">
+
+        <i class="fab fa-whatsapp"></i>
+
+        <form action="#">
+
+            <p>
+            
+                <label>
+                <span>Quiero recibir alertas sobre el estado de mi pedido por WhatsApp</span>
+                <input class="with-gap" name="group3" type="radio"  />
+                </label>
+            
+            </p>
+
+        </form>
+        
+    </div>
+
+    <div class="row">
+    
+        <div class="col-6">
+
+            <p class="text-muted">Calle</p>
+
+            <input type="text" class="form-control">
+        
+        </div>
+
+        <div class="col-3">
+
+            <p class="text-muted">N° Exterior</p>
+
+            <input type="text" class="form-control">
+        
+        </div>
+
+        <div class="col-3">
+
+            <p class="text-muted">N° Interior</p>
+
+            <input type="text" class="form-control">
+        
+        </div>
+    
+    </div>
+
+    <div class="row">
+    
+        <div class="col-6">
+
+            <p class="text-muted">Codigo Postal</p>
+
+            <input type="text" class="form-control">
+        
+        </div>
+
+        <div class="col-6">
+
+            <p class="text-muted">Estado</p>
+
+            <select class="form-control">
+            
+                <option> Estado de Mexico </option>
+                <option>San Luis Potosi</option>
+                <option>Cdmx</option>
+                <option>Tu culo</option>
+
+            </select>
+        
+        </div>
+    
+    </div>
+
+    <div class="row">
+    
+        <div class="col-6">
+
+            <p class="text-muted">Ciudad</p>
+
+            <select class="form-control">
+            
+                <option>Ecatepec de Morelos</option>
+                <option>Azcapotzalco</option>
+            
+            </select>
+        
+        </div>
+
+        <div class="col-6">
+
+            <p class="text-muted">Delegacion</p>
+
+            <select class="form-control">
+            
+                <option>Ecatepec de Morelos</option>
+
+            </select>
+        
+        </div>
+    
+    </div>
+
+    <div class="row">
+    
+        <div class="col-6">
+
+            <p class="text-muted">Colonia</p>
+
+            <select class="form-control">
+            
+                <option>Ampliacion Joyas de Ecatepec</option>
+                <option>Fernando de Alba</option>
+                <option>Valle de Anahuac</option>
+                <option>La Estrella</option>
+                <Option>San Agustin</Option>
+            
+            </select>
+        
+        </div>
+
+        <div class="col-6">
+
+            <p class="text-muted">Entre la calle (opcional)</p>
+
+            <input type="text" class="form-control">
+        
+        </div>
+    
+    </div>
+
+    <div class="row">
+    
+        <div class="col-6">
+
+            <p class="text-muted">Y la calle (opcional)</p>
+
+            <input type="text" class="form-control">
+        
+        </div>
+
+        <div class="col-6">
+
+            <p class="text-muted">Instrucciones Especiales</p>
+
+            <input type="text" class="form-control">
+        
+        </div>
+    
+    </div>
+
+    <div class="form-inline">
+
+        <i class="fas fa-map-marker-alt"></i>
+
+        <form action="#">
+
+            <p>
+            
+                <label>
+                <span>Usar como direccion predeterminada</span>
+                <input class="with-gap" name="group3" type="radio"  />
+                </label>
+            
+            </p>
+
+        </form>
+        
+    </div>
+
+    <div class="float-right">
+    
+        <button class="btn btn-block">
+            
+            Guardar Direccion
+
+        </button>
+
+    </div>
+
+</div>
